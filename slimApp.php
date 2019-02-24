4468916199ac07422b0b29f056808217a4b92f20
martin.cuba459 <martin.cuba459@gmail.com>
2019-02-24 17:01:45 -0300

    find similar

--- slimApp.php
+++ slimApp.php
@@ -39,13 +39,24 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
     return $response->withJson($args);
 });
 
-// Query example to a Postgres Database
+// Select All pokemon to a Postgres Database
 $app->get('/selectAll', function (Request $request, Response $response, array $args) {
     $directory = $this->get('settings')['image_url'];
 	$query = $this->db->query('select p.id, p.nombre,  CONCAT(\''.$directory.'\',p."nombreArchivo")
      as nombreArchivo from "pokemon" p');
     return $response->withJson($query->get());
 });
+
+//Get similar
+$app->get('/similar/{idPokemon}[/{count}]', function (Request $request, Response $response, array $args) {
+    $count=10;
+    if(isset( $args['count'])){
+         $count=$args['count'];
+    }
+	$query = $this->db->query('select ObtenerNPokemonsSimilares('.$count.', '.$args['idPokemon'].');');
+    return $response->withJson($query->get());
+});
+
 
 // Upload an image
 $app->post('/upload', function(Request $request, Response $response) {
