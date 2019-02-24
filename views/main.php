f42074a0ac5c104d6a3d73ec9b4107d1496fca19
Valentín Costa <valentincostam@gmail.com>
2019-02-24 15:20:19 -0300

    Modificación de algunos textos.

--- views/main.php
+++ views/main.php
@@ -10,6 +10,7 @@
 <body>
   <div id="app">
     <section id="busqueda">
+      <h2>Elige un pokemon</h2>
       
       <ul class="lista-pokemon">
         <!-- <li v-for="name in imagesFilenames" :key="name">
@@ -22,46 +23,43 @@
       </ul>
       
       <div class="toolbar">
-        <file-upload
-          ref="upload"
-          v-model="files"
-          class="file-input"
-          post-action="./upload"
-          @input-file="inputFile"
-          @input-filter="inputFilter"
-        >
-          Agregar pokemon...
-        </file-upload>
+        <div class="upload-area">
+          <file-upload
+            ref="upload"
+            v-model="files"
+            class="file-input"
+            post-action="./upload"
+            @input-file="inputFile"
+            @input-filter="inputFilter"
+          >
+            Seleccionar pokemon...
+          </file-upload>
+    
+          <a
+            v-show="!$refs.upload || !$refs.upload.active"
+            @click.prevent="$refs.upload.active = true"
+            type="button"
+          >
+            Subir y buscar
+          </a>
   
-        <a
-          v-show="!$refs.upload || !$refs.upload.active"
-          @click.prevent="$refs.upload.active = true"
-          type="button"
-        >
-          Subir y buscar
-        </a>
-
-        <a
-          v-show="$refs.upload && $refs.upload.active"
-          @click.prevent="$refs.upload.active = false"
-          type="button"
-        >
-          Detener subida
-        </a>
+          <a
+            v-show="$refs.upload && $refs.upload.active"
+            @click.prevent="$refs.upload.active = false"
+            type="button"
+          >
+            Detener subida
+          </a>
+        </div>
         <a id="buscar">Buscar pokemons similares</a>
       </div>
     </section>
     <section id="resultados">
-      <h2>Resultados XYZ</h2>
+      <h2>Resultados</h2>
     </section>
   </div>
 </body>
-<script
-  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/vue-upload-component"></script>
 <script src="main.js"></script>
-<!-- <script src="prueba.js"></script> -->
 </html>
