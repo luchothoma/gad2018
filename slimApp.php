27b0f12786b727b783f8f6eb7765747a9c0944c8
Luciano Thoma <luchothoma@gmail.com>
2019-02-27 00:41:23 -0300

    Inserta pokemon (img) subido a DB y retorna sus datos necesarios

--- slimApp.php
+++ slimApp.php
@@ -66,11 +66,24 @@ $app->post('/upload', function(Request $request, Response $response) {
 
     if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
     	$directory = $this->get('settings')['upload_directory'];
+    	$public_directory = $this->get('settings')['image_url'];
         $filename = moveUploadedFile($directory, $uploadedFile);
+        $nombre = 'NOMBREharcodeado';
+        $vector = createCharacteristicVector($directory.'/'.$filename);
+
+        $data = array(
+		    'nombre' => $nombre,
+		    'nombreArchivo' => $filename,
+		);
+
+		foreach ($vector as $index=> $value) {
+			$data['c'.($index+1)] = $value;
+		}
 
         return $response->withJson([
-        	'filename' => $filename,
-        	'vector' => createCharacteristicVector($directory.'/'.$filename),
+        	'nombre' => $nombre,
+        	'nombreArchivo' => $filename,
+        	'id' => $this->db->table('pokemon')->insert($data),
         ]);
     }
     else
@@ -80,17 +93,30 @@ $app->post('/upload', function(Request $request, Response $response) {
 
 // prueba
 $app->get('/test', function(Request $request, Response $response) {
-    	$directory = $this->get('settings')['upload_directory'];
-        $filename = "abra.png";
-        
-        $ti =  microtime(true);
-        $v = createCharacteristicVector($directory.'/'.$filename);
-        $tf =  microtime(true);
+	/*
+	$directory = $this->get('settings')['upload_directory'];
+    $filename = "abra.png";
+    
+    $ti =  microtime(true);
+    $v = createCharacteristicVector($directory.'/'.$filename);
+    $tf =  microtime(true);
 
-        return $response->withJson([
-        	'vector' => $v,
-        	'tiempo' => (($tf-$ti)/1000)
-        ]);
+    return $response->withJson([
+    	'vector' => $v,
+    	'tiempo' => (($tf-$ti)/1000)
+    ]);
+    */
+    /*
+    $data = [
+    	'nombre' => "Nombre",
+    	'nombreArchivo' => "abra.png",
+    	'c1' => 0.1,'c2' => 0.1,'c3' => 0.1,'c4' => 0.1,'c5' => 0.1,'c6' => 0.1,'c7' => 0.1,'c8' => 0.1,'c9' => 0.1,'c10' => 0.1,
+    	'c11' => 0.1,'c11' => 0.1,'c13' => 0.1,'c14' => 0.1,'c15' => 0.1,'c16' => 0.1,'c17' => 0.1,'c18' => 0.1,'c19' => 0.1,'c20' => 0.1,
+    	'c21' => 0.1,'c22' => 0.1,'c23' => 0.1,'c24' => 0.1,'c25' => 0.1,'c26' => 0.1,'c27' => 0.1,'c28' => 0.1,'c29' => 0.1,'c30' => 0.1,
+    	'c31' => 0.1,'c32' => 0.1,'c33' => 0.1,'c34' => 0.1,'c35' => 0.1,'c36' => 0.1,'c37' => 0.1,'c38' => 0.1,'c39' => 0.1,'c40' => 0.1,
+    ];
+    return $response->withJson(['id' => $this->db->table('pokemon')->insert($data)]);
+    */
 });
 
 
